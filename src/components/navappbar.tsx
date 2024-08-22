176d598a27f3146d15f164109c9967b377577d3b
David Jeske <dcjeske@tamu.edu>
2024-08-22 10:19:38 -0500

    Cleaned up many components to hopefully allow even easier new implementations

--- src/components/navappbar.tsx
+++ src/components/navappbar.tsx
@@ -23,7 +23,7 @@ import { useTheme } from "@mui/material/styles";
 import LoginIcon from "@mui/icons-material/Login";
 
 const badge = "🔫";
-const initLogin = true;
+const initLogin = false;
 
 export default function PrimarySearchAppBar() {
   //getting a navigate hook to go between routes
@@ -108,7 +108,7 @@ export default function PrimarySearchAppBar() {
   }));
 
   //combines all the pieces of the styled components
-  const searchBar = ( 
+  const searchBar = (
     <Search>
       <SearchIconWrapper>
         <SearchIcon />
@@ -116,11 +116,12 @@ export default function PrimarySearchAppBar() {
       <StyledInput
         placeholder="Search…"
         type="search"
-        onKeyDown={(e) => {if(e.key==="Enter")
-          handleSearch(e);}}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") handleSearch(e);
+        }}
         fullWidth
       />
-    </Search>  
+    </Search>
   );
 
   function handleSearch(event) {
@@ -129,7 +130,7 @@ export default function PrimarySearchAppBar() {
 
   //#endregion
 
-  //#region usermenu (title is wip)
+  //#region usermenu
 
   //state for determining if logged in
   //FIXME: this should probably be handled differently, this was mostly a proof of concept
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import NavAppBar from "../components/navappbar";
+import { Grid, Divider } from "@mui/material";
 
 export default function Layout() {
   return (
     <>
       <NavAppBar />
-      <Outlet />
+      <Grid container direction="column" minWidth={"500px"} sx={{ p: 5 }}>
+        <Divider sx={{ m: 5, display: "inline-block" }} />
+        <Outlet />
+      </Grid>
       <img
         src={"src/assets/legallyRequiredPNG.png"}
         width="180"
