2dda4917bdafed47dc71f6857ee606540d8cc4c3
David Jeske <dcjeske@tamu.edu>
2024-08-22 10:13:45 -0500

    Revert "normalized some spacings"
    This reverts commit 0a5254cac535c7c0365258352975a38a09aa23ad.
    
    Previous commit was incorrectly named and pushed

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,23 +1,40 @@
-import { Typography, Card, Divider } from "@mui/material";
+import { Typography, Card, Grid, Divider } from "@mui/material";
 
 export default function Home() {
-  
+  //ignore this
   const loremIpsum =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ullamco laboris nisi utaliquip ex ea commodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiat nullapariatur. Excepteur sint occaecat cupidatat non proident, sunt inculpa qui officia deserunt mollit anim id est laborum.";
 
-  return (
-    <Card sx={{ p: 5, m: 5 }}>
-
+  const textCard = (
+    <Card
+      sx={{
+        width: "80%",
+        p: 5,
+        m: 4,
+      }}
+    >
       <Typography variant="h2" align="center">
         Home Page
       </Typography>
-
       <Divider sx={{ my: 2 }} />
-
-      <Typography variant="body1" align="justify">
+      <Typography align="justify">
         {loremIpsum} {loremIpsum} {loremIpsum} {loremIpsum}
       </Typography>
-
     </Card>
+  );
+
+  return (
+    <>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        minWidth={"500px"}
+      >
+        <Divider sx={{ m: 6 }} />
+        {textCard}
+      </Grid>
+    </>
   );
 }
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,15 +1,11 @@
 import { Outlet } from "react-router-dom";
 import NavAppBar from "../components/navappbar";
-import { Grid, Divider } from "@mui/material";
 
 export default function Layout() {
   return (
     <>
       <NavAppBar />
-      <Grid container direction="column" minWidth={"500px"} sx={{ p: 5 }}>
-        <Divider sx={{ m: 5, display: "inline-block" }} />
-        <Outlet />
-      </Grid>
+      <Outlet />
       <img
         src={"src/assets/legallyRequiredPNG.png"}
         width="180"
