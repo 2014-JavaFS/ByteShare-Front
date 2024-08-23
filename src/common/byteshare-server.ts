d497cc2ae808038cc80f180cf6ccb43d57ba1823
asaf ahmed <asafbishoy@gmail.com>
2024-08-23 14:44:08 -0500

    fixed axios

--- src/common/byteshare-server.ts
+++ src/common/byteshare-server.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const amsServer = axios.create({
-    baseURL: "http://ec2-3-141-202-46.us-east-2.compute.amazonaws.com:9999",
+    baseURL: "http://ec2-3-141-202-46.us-east-2.compute.amazonaws.com:8888",
     headers: {
         Accept: "application/json",
         "Content-Type": "application/json"
