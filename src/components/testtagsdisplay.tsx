5a581ab31d796045b7dda1eaee448231f12e8b1a
Ethan Vrooman <98380474+env7295@users.noreply.github.com>
2024-08-27 14:07:43 -0500

    Added another todo comment

--- src/components/testtagsdisplay.tsx
+++ src/components/testtagsdisplay.tsx
@@ -2,7 +2,8 @@ import { Box, Chip, Grid, Stack } from "@mui/material";
 
 export default function TestTagBlock(){
 
-    //TODO :  this array should be input from props or some backend call, hardcoding for now
+    // TODO :  this array should be input from props or some backend call, hardcoding for now
+    // this array of strings should be received from the backend findAllTagNamesByRecipe method.
     const tagArray: string[] = [ "dummyTagOne", "dummyTagThree","dummyTagTwo",]
     return(
         <Grid>
