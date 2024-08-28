0c30170683351f633e36bd0b16f063bf28200249
Iskender <zeres23kar@gmail.com>
2024-08-28 15:13:11 -0500

    developed a feature that displays ingredientlist based on recipe id

--- src/common/byteshare-server.ts
+++ src/common/byteshare-server.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const bsServer = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "http://ec2-3-141-202-46.us-east-2.compute.amazonaws.com:8888",
     headers: {
         Accept: "application/json",
         "Content-Type": "application/json"
